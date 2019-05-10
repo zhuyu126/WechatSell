891ae8530975a83f8a3a546ba15671c945ad228b
robinson <zhuyu553@gmail.com>
2019-05-11 00:59:18 +0800

    订单信息处理

--- src/main/java/com/wechat/sell/utils/ResultVOUtil.java
+++ src/main/java/com/wechat/sell/utils/ResultVOUtil.java
@@ -5,7 +5,20 @@ import com.wechat.sell.VO.ResultVO;
 import java.util.Map;
 
 public class ResultVOUtil {
-    public static ResultVO<Map<String, String>> success(Map<String, String> map) {
-        return null;
+    public static ResultVO success(Object object){
+        ResultVO resultVO=new ResultVO();
+        resultVO.setCode(0);
+        resultVO.setData(object);
+        resultVO.setMsg("成功");
+        return resultVO;
+    }
+    public static ResultVO success(){
+        return success(null);
+    }
+    public static ResultVO error(Integer code,String msg){
+        ResultVO resultVO=new ResultVO();
+        resultVO.setCode(code);
+        resultVO.setMsg(msg);
+        return resultVO;
     }
 }
---
+++ src/test/java/com/wechat/sell/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.wechat.sell.service.impl;
+
+import com.wechat.sell.dto.OrderDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class OrderServiceImplTest {
+
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    private final String BUYER_OPENID = "110110";
+
+    private final String ORDER_ID = "1497183332311989948";
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void findOne() {
+    }
+
+    @Test
+    public void findList() {
+        PageRequest request = new PageRequest(0,2);
+        Page<OrderDTO> orderDTOPage = orderService.findList(BUYER_OPENID, request);
+        Assert.assertNotEquals(0, orderDTOPage.getTotalElements());
+    }
+
+    @Test
+    public void cancel() {
+    }
+
+    @Test
+    public void finished() {
+    }
+
+    @Test
+    public void paid() {
+    }
+}
